2b0bc56d0d93ad3c888b41f6b74d67e29d6f0537
Fahmida-68 <antanafahmida2013@gmail.com>
2026-06-29 19:34:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-# main.py
-import sys
 from lexer import Lexer
 from parser import Parser
 from interpreter import Interpreter
 
-def run_code(source_code):
+def run_code(input_text):
     try:
-        lexer = Lexer(source_code)
+        lexer = Lexer(input_text)
         tokens = lexer.make_tokens()
         
         parser = Parser(tokens)
         ast = parser.parse()
         
         interpreter = Interpreter()
-        interpreter.execute(ast)
-        
-        return "\n".join(interpreter.console_output)
+        output = interpreter.visit(ast)
+        return output
     except Exception as e:
         return f"[Compiler Error]: {str(e)}"
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        with open(filename, 'r') as file:
-            code = file.read()
-        print(run_code(code))
-    else:
-        print("FLang Engine Active. Run with filename parameter blueprint (e.g., python main.py program.fl)")
