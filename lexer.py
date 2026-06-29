124a4a8939c87583dbc0856d9254f9b053a04060
Fahmida-68 <antanafahmida2013@gmail.com>
2026-06-29 18:05:33 +0600

    Refactor lexer to use Lexer class structure

--- lexer.py
+++ lexer.py
@@ -1,51 +1,81 @@
 # lexer.py
-from tokens import Token, TOKEN_INT, TOKEN_PLUS, TOKEN_MINUS, TOKEN_MUL, TOKEN_DIV, TOKEN_PRINT, TOKEN_EOF
+from tokens import *
 
-def lexer(filename):
-    with open(filename, 'r') as f:
-        text = f.read()
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos] if self.text else None
 
-    tokens = []
-    pos = 0
-    while pos < len(text):
-        char = text[pos]
+    def advance(self):
+        self.pos += 1
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
 
-        # Space, Tab, Newline skip korar jonno
-        if char in ' \t\n\r':
-            pos += 1
-            continue
+    def make_tokens(self):
+        tokens = []
+        while self.current_char is not None:
+            if self.current_char in ' \t\n\r':
+                self.advance()
+            elif self.current_char.isdigit():
+                tokens.append(self.make_number())
+            elif self.current_char.isalpha():
+                tokens.append(self.make_identifier())
+            elif self.current_char == '"' or self.current_char == "'":
+                tokens.append(self.make_string())
+            elif self.current_char == '+':
+                tokens.append(Token(TOKEN_PLUS)), self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TOKEN_MINUS)), self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TOKEN_MUL)), self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TOKEN_DIV)), self.advance()
+            elif self.current_char == '=':
+                tokens.append(self.make_equals())
+            elif self.current_char == '<':
+                tokens.append(Token(TOKEN_LT)), self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TOKEN_LPAREN)), self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TOKEN_RPAREN)), self.advance()
+            elif self.current_char == '{':
+                tokens.append(Token(TOKEN_LBRACE)), self.advance()
+            elif self.current_char == '}':
+                tokens.append(Token(TOKEN_RBRACE)), self.advance()
+            else:
+                raise Exception(f"Illegal Character: '{self.current_char}'")
         
-        # KEYWORD CHECK: "bol" keyword khujchi print korar jonno
-        if text[pos:pos+3] == "bol" and (pos+3 >= len(text) or text[pos+3] in ' \t\n\r'):
-            tokens.append(Token(TOKEN_PRINT))
-            pos += 3
-            continue
+        tokens.append(Token(TOKEN_EOF))
+        return tokens
 
-        # Operators Check
-        if char == '+':
-            tokens.append(Token(TOKEN_PLUS))
-            pos += 1
-        elif char == '-':
-            tokens.append(Token(TOKEN_MINUS))
-            pos += 1
-        elif char == '*':
-            tokens.append(Token(TOKEN_MUL))
-            pos += 1
-        elif char == '/':
-            tokens.append(Token(TOKEN_DIV))
-            pos += 1
-        
-        # Number Check (Multiple digits handle korar jonno loop)
-        elif char.isdigit():
-            num = ''
-            while pos < len(text) and text[pos].isdigit():
-                num += text[pos]
-                pos += 1
-            tokens.append(Token(TOKEN_INT, int(num)))
-        else:
-            # Jodi emon kichu pay jeta amader syntax e nai
-            print(f"[Lexer Error]: Invalid character '{char}' at position {pos}")
-            pos += 1
-            
-    tokens.append(Token(TOKEN_EOF))
-    return tokens
+    def make_number(self):
+        num_str = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            num_str += self.current_char
+            self.advance()
+        return Token(TOKEN_INT, int(num_str))
+
+    def make_string(self):
+        quote_char = self.current_char
+        string_str = ''
+        self.advance()
+        while self.current_char is not None and self.current_char != quote_char:
+            string_str += self.current_char
+            self.advance()
+        self.advance() # Skip trailing quote
+        return Token(TOKEN_STRING, string_str)
+
+    def make_identifier(self):
+        id_str = ''
+        while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
+            id_str += self.current_char
+            self.advance()
+        tok_type = TOKEN_KEYWORD if id_str in KEYWORDS else TOKEN_IDENTIFIER
+        return Token(tok_type, id_str)
+
+    def make_equals(self):
+        self.advance()
+        if self.current_char == '=':
+            self.advance()
+            return Token(TOKEN_EE)
+        return Token(TOKEN_EQUALS)
