c0ecd02d8f0ac5f2202eab5a1d3e8fbac2d40735
Fahmida-68 <antanafahmida2013@gmail.com>
2026-06-30 00:29:33 +0600

    Refactor token definitions and class structure

--- tokens.py
+++ tokens.py
@@ -1,38 +1,22 @@
 # tokens.py
+TOKEN_INT = 'INT'
+TOKEN_STRING = 'STRING'
+TOKEN_IDENTIFIER = 'IDENTIFIER'
+TOKEN_KEYWORD = 'KEYWORD'
+TOKEN_PLUS = 'PLUS'
+TOKEN_MINUS = 'MINUS'
+TOKEN_MUL = 'MUL'
+TOKEN_DIV = 'DIV'
+TOKEN_EQUALS = 'EQUALS'
+TOKEN_EE = 'EE'
+TOKEN_LT = 'LT'
+TOKEN_LPAREN = 'LPAREN'
+TOKEN_RPAREN = 'RPAREN'
+TOKEN_LBRACE = 'LBRACE'
+TOKEN_RBRACE = 'RBRACE'
+TOKEN_EOF = 'EOF'
 
-# Token Types Constants
-TOKEN_INT        = 'INT'
-TOKEN_PLUS       = 'PLUS'
-TOKEN_MINUS      = 'MINUS'
-TOKEN_MUL        = 'MUL'
-TOKEN_DIV        = 'DIV'
-TOKEN_EOF        = 'EOF'
-
-# Comparison & Assignment Operators
-TOKEN_EQUALS     = 'EQUALS'       # =
-TOKEN_EE         = 'EE'           # ==
-TOKEN_LT         = 'LT'           # <
-TOKEN_LBRACE     = 'LBRACE'       # {
-TOKEN_RBRACE     = 'RBRACE'       # }
-TOKEN_LPAREN     = 'LPAREN'       # (
-TOKEN_RPAREN     = 'RPAREN'       # )
-
-# FLang Keywords Mapping
-TOKEN_IDENTIFIER = 'IDENTIFIER'   # For variable names like a, b, x
-TOKEN_STRING     = 'STRING'       # For text inside quotes like "Hello World"
-TOKEN_KEYWORD    = 'KEYWORD'      # For structural keywords
-
-# List of registered Keywords 
-KEYWORDS = [
-    'shuru',
-    'flang',
-    'shesh',
-    'bol',
-    'rakho',
-    'jotokkhon',
-    'jodi',
-    'natho'
-]
+KEYWORDS = ['shuru', 'flang', 'shesh', 'bol', 'rakho', 'jodi', 'natho', 'jotokkhon']
 
 class Token:
     def __init__(self, type_, value=None):
@@ -40,6 +24,5 @@ class Token:
         self.value = value
 
     def __repr__(self):
-        if self.value is not None: 
-            return f'{self.type}:{self.value}'
+        if self.value is not None: return f'{self.type}:{self.value}'
         return f'{self.type}'
