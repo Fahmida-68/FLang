89075d3fc2af61d1c80a0d1dad6ff2223166d9ee
Fahmida-68 <antanafahmida2013@gmail.com>
2026-06-29 18:00:09 +0600

    Add token types for comparison and assignment
    Added comparison and assignment operator token types.

--- tokens.py
+++ tokens.py
@@ -1,19 +1,16 @@
 # tokens.py
 
 # Token Types Constants
-TOKEN_INT    = 'INT'
-TOKEN_PLUS   = 'PLUS'
-TOKEN_MINUS  = 'MINUS'
-TOKEN_MUL    = 'MUL'
-TOKEN_DIV    = 'DIV'
-TOKEN_PRINT  = 'PRINT'
-TOKEN_EOF    = 'EOF'
+TOKEN_INT        = 'INT'
+TOKEN_PLUS       = 'PLUS'
+TOKEN_MINUS      = 'MINUS'
+TOKEN_MUL        = 'MUL'
+TOKEN_DIV        = 'DIV'
+TOKEN_EOF        = 'EOF'
 
-class Token:
-    def __init__(self, type_, value=None):
-        self.type = type_
-        self.value = value
-
-    def __repr__(self):
-        if self.value is not None: return f'{self.type}:{self.value}'
-        return f'{self.type}'
+# Comparison & Assignment Operators
+TOKEN_EQUALS     = 'EQUALS'       # =
+TOKEN_EE         = 'EE'           # ==
+TOKEN_LT         = 'LT'           # <
+TOKEN_LBRACE     = 'LBRACE'       # {
+TOKEN_
