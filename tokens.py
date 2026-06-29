081611dbf5f1e98e1806b6e2e59061b0a8c6e3ee
Fahmida-68 <antanafahmida2013@gmail.com>
2026-06-29 18:02:58 +0600

    Refactor token constants and add keywords

--- tokens.py
+++ tokens.py
@@ -13,4 +13,33 @@ TOKEN_EQUALS     = 'EQUALS'       # =
 TOKEN_EE         = 'EE'           # ==
 TOKEN_LT         = 'LT'           # <
 TOKEN_LBRACE     = 'LBRACE'       # {
-TOKEN_
+TOKEN_RBRACE     = 'RBRACE'       # }
+TOKEN_LPAREN     = 'LPAREN'       # (
+TOKEN_RPAREN     = 'RPAREN'       # )
+
+# FLang Keywords Mapping
+TOKEN_IDENTIFIER = 'IDENTIFIER'   # For variable names like a, b, x
+TOKEN_STRING     = 'STRING'       # For text inside quotes like "Hello World"
+TOKEN_KEYWORD    = 'KEYWORD'      # For structural keywords
+
+# List of registered Keywords
+KEYWORDS = [
+    'shuru',
+    'flang',
+    'shesh',
+    'bol',
+    'rakho',
+    'jotokkhon',
+    'jodi',
+    'natho'
+]
+
+class Token:
+    def __init__(self, type_, value=None):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        if self.value is not None: 
+            return f'{self.type}:{self.value}'
+        return f'{self.type}'
